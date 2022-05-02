c1efd929331b297205dfc30576a92996b8e1bcc7
SanariaM <sanariamky@gmail.com>
2022-05-02 00:07:09 -0400

    Render reservation buttons (disabled if already taken)

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ async function getCurrentUser() {
 
   return await response.json();
 }
+
+// Creates an element from a given HTML string
+function newElement(html) {
+  const template = document.createElement("template");
+  template.innerHTML = html.trim();
+  return template.content.firstChild;
+}
+
+// Convert the given date object to a string in MySQL's date format
+function dateToMysqlFormat(date = new Date()) {
+  return `${date.getFullYear()}-${
+    date.getMonth() + 1
+  }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
